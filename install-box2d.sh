2950fba75112aaac0955eaa8a19154d8b0cf6386
Adrien Duval <adrien.duval.bonnet@gmail.com>
2021-02-06 19:22:10 +0100

    Fix box2d script install prefix

--- install-box2d.sh
+++ install-box2d.sh
@@ -5,9 +5,6 @@ cd box2d
 mkdir build
 cd build
 
-cmake -DBOX2D_BUILD_TESTBED=OFF -DBUILD_SHARED_LIBS=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DBOX2D_BUILD_TESTBED=OFF -DBUILD_SHARED_LIBS=ON ..
 make
-sudo make install
-
-cd ../..
-rm -rf box2d
+sudo make install && cd ../.. && rm -rf box2d
