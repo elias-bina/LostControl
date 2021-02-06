20c5235400e8bdc585873c98c23078306c114a6c
Elias Bina <elias.bina@grenoble-inp.org>
2021-02-06 14:50:27 +0100

    Hello !

---
+++ include/main.h
@@ -0,0 +1,4 @@
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <set>
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+#include "main.h"
+
+
+int main()
+{
+
+    std::cout << "Hello World !" ;
+
+    return 0;
+}
