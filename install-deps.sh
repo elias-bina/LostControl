9d4c3c501dcbed27617c7cad856a8c7f29ecd962
Adrien Duval <adrien.duval.bonnet@gmail.com>
2021-02-11 19:11:25 +0100

    Ajout de la librarie Thor au projet

---
+++ install-deps.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+mkdir -p deps
+cd deps
+
+echo "Installing box2d"
+if ! git clone https://github.com/erincatto/box2d.git
+then
+    echo "Cannot download Box2D"
+    exit 1
+fi
+cd box2d
+mkdir -p build
+cd build
+
+if ! cmake -DCMAKE_INSTALL_PREFIX=/usr -DBOX2D_BUILD_TESTBED=ON -DBUILD_SHARED_LIBS=ON .. && make
+then
+    echo "Cannot build Box2D"
+    exit 1
+fi
+if ! sudo make install && cd ../..
+then
+    echo "Cannot install Box2D"
+    exit 1
+fi
+
+echo "Installing Thor"
+if ! git clone https://github.com/Bromeon/Thor.git
+then
+    echo "Cannot download Thor"
+    exit 1
+fi
+
+cd Thor
+mkdir -p build
+cd build
+
+if ! cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON .. && make
+then 
+    echo "Cannot build Thor"
+    exit 1
+fi
+if ! sudo make install && cd ../..
+then
+    echo "Cannot install Thor"
+    exit 1
+fi
+
+cd ..
+
+echo "End of installation, you can safely remove deps folder if you want to"
